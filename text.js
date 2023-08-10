776618aad5620a4a30006444b9d8ff148b39a78b
Bryan Delgado <bryand9970@gmail.com>
2023-08-10 10:55:21 -0500

    Web page Desktop Version - Lab 5

---
+++ text.js
@@ -0,0 +1,26 @@
+var typed = new Typed('#element1', {
+    strings: ['I received a job offer mid-course, and the subjects I learned were current, if not more so, in the company I joined. I honestly feel I got every penny’s worth. '],
+    typeSpeed: 80,
+    });
+
+var typed = new Typed('#element2', {
+    strings: [' Awesome teaching support from TAs who did the bootcamp themselves. Getting guidance from them and learning from their experiences was easy. '],
+    typeSpeed: 80,
+    });
+
+var typed = new Typed('#element3', {
+    strings: ['An overall wonderful and rewarding experience.'],
+    typeSpeed: 80,
+    });
+
+
+var typed = new Typed('#element4', {
+    strings: ['The team was very supportive and kept me motivated.'],
+    typeSpeed: 80,
+    });    
+    
+var typed = new Typed('#element5', {
+    strings: ['Such a life-changing experience. Highly recommended! '],
+    typeSpeed: 80,
+    });    
+        
